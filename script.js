ef03f2f872a291d015c063c6754c77badd0dbe61
arekusandaken <32167521+arekusandaken@users.noreply.github.com>
2018-03-24 20:44:02 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -76,3 +76,45 @@ function likeShow()
 		});
 	}, 1000);
 }
+
+
+
+(function () {
+			var body = document.getElementsByTagName('body')[0];
+
+			var switcherRU = document.getElementById('switcher-ru');
+			var switcherEN = document.getElementById('switcher-en');
+
+			// Заранее считаем срок хранения кук
+			var expires = new Date();
+			expires.setDate(expires.getDate() + 366);
+			expires.toUTCString();
+
+			// Русский и английский заголовки
+			var titleRU = document.getElementById('title-ru');
+			var titleEN = document.getElementById('title-en');
+			var title = {
+				ru: titleRU.getAttribute('content'),
+				en: titleEN.getAttribute('content')
+			}
+
+			var switchLang = function () {
+				var lang = this.id
+				lang = lang.replace('switcher-', '');
+
+				// Меняем класс для BODY
+				body.className = lang;
+
+				// Записываем куки
+				document.cookie = [
+					'aplang=' + lang
+					, '; expires=' + expires // используем expires, max-age не поддерживается в ИЕ
+					, '; path=/'
+					, '; domain=' + document.location.host
+				].join('');
+				document.title = title[lang];
+			}
+
+			switcherRU.onclick = switchLang;
+			switcherEN.onclick = switchLang;
+		})();
